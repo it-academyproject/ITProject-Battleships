82ee052d0b9891eec3b85aa77604e43f9d3c750f
gerihal <gerahernan.al@gmail.com>
2020-09-30 17:33:33 +0200

    added player into challenge

--- ITProject-Battleships/Models/Challenge.cs
+++ ITProject-Battleships/Models/Challenge.cs
@@ -23,5 +23,9 @@ namespace ITProject_Battleships.Models
 
         public int TurnsNumbers { get; set; }
 
+        public Player Player1 { get; set; }
+
+        public Player Player2 { get; set; }
+
     }
 }
