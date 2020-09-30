272e9ac10cf67b4b8b54df139db4465c75b26b68
gerihal <gerahernan.al@gmail.com>
2020-09-30 15:20:08 +0200

    Added Player Model

--- ITProject-Battleships/Models/Player.cs
+++ ITProject-Battleships/Models/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,27 @@ namespace ITProject_Battleships.Models
 {
     public class Player
     {
+        [Key]
+        public int PlayerId { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime CreatedUserData { get; set; }
+
+        public DateTime LastLogin { get; set; }
+
+        public string Nickname { get; set; }
+
+        public int BattlesWon { get; set; }
+
+        public int BattlesLost { get; set; }
+
+        public int BattlesDraw { get; set; }
+
+        public string ProfilePicture { get; set; }
+
+        public string Description { get; set; }
     }
 }
