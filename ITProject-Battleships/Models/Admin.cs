f4231e20cf007ce31916acabfedc3349e6e68113
gerihal <gerahernan.al@gmail.com>
2020-09-30 15:15:49 +0200

    Creating Admin Model

---
+++ ITProject-Battleships/Models/Admin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITProject_Battleships.Models
+{
+    public class Admin
+    {
+        [Key]
+        public int AdminId { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime CreatedUserData { get; set; }
+
+        public DateTime LastLogin { get; set; }
+
+    }
+}
---
+++ ITProject-Battleships/Models/BattleField.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITProject_Battleships.Models
+{
+    public class BattleField
+    {
+    }
+}
