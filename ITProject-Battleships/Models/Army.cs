c1f6cc949087a49d85b9ce92e432842d489f64a8
Ofelia <ofeliacocucci@outlook.com>
2020-10-06 13:15:03 +0200

    Added Army Ships, Added new migration and updated database.

--- ITProject-Battleships/Models/Army.cs
+++ ITProject-Battleships/Models/Army.cs
@@ -10,7 +10,11 @@ namespace ITProject_Battleships.Models
     public class Army
     {
         public int ArmyId { get; set; }
-        public ShipType ArmyShips { get; set; }
+        public int Boats { get; set; }
+        public int BattleShips { get; set; }
+        public int Submarines { get; set; }
+        public int Vessels { get; set; }
+        public int Carriers { get; set; }
         public virtual BattleField BattleField { get; set; }
     }
 }
