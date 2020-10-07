5ff332be6ac8929a9a6351fd69747651c80cfe58
gerihal <gerahernan.al@gmail.com>
2020-10-07 10:28:18 +0200

    added jwt package nuget and configure startup class with auth

--- ITProject-Battleships/Startup.cs
+++ ITProject-Battleships/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ITProject_Battleships.Data;
 using ITProject_Battleships.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,8 +12,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Text;
 
 namespace ITProject_Battleships
 {
@@ -29,6 +32,24 @@ namespace ITProject_Battleships
         public void ConfigureServices ( IServiceCollection services )
         {
             services.AddControllers();
+
+            services.AddAuthentication ( JwtBearerDefaults.AuthenticationScheme )
+                .AddJwtBearer ( options =>
+                  {
+                      options.TokenValidationParameters = new TokenValidationParameters
+                      {
+                          ValidateIssuer = true,
+                          ValidateAudience = true,
+                          ValidateLifetime = true,
+                          ValidateIssuerSigningKey = true,
+                          ValidIssuer = Configuration["Jwt:Issuer"],
+                          ValidAudience = Configuration["Jwt:Issuer"],
+                          IssuerSigningKey = new SymmetricSecurityKey(
+                              Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+
+                      };
+                  } );
+                
 
             services.AddDbContext<BattleContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("ConnStr")));
@@ -52,6 +73,8 @@ namespace ITProject_Battleships
             app.UseHttpsRedirection ();
 
             app.UseRouting ();
+
+            app.UseAuthentication ();
 
             app.UseAuthorization ();
 
