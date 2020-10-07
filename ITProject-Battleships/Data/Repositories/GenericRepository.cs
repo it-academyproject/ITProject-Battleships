7ca04754be7c654146cc9dbd8ecfb538abb9bd34
gerihal <gerahernan.al@gmail.com>
2020-10-07 12:37:16 +0200

    Create GenericRepository

--- ITProject-Battleships/Data/Repositories/GenericRepository.cs
+++ ITProject-Battleships/Data/Repositories/GenericRepository.cs
@@ -1,11 +1,38 @@
-﻿using System;
+﻿using ITProject_Battleships.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ITProject_Battleships.Data.Repositories
 {
-    public class GenericRepository
+    public class GenericRepository<TEntity, TContext> : IRepository<TEntity>
+        where TEntity : class, IEntity
+        where TContext : DbContext
     {
+        public Task<TEntity> Add ( TEntity entity )
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Task<TEntity> Get ( int id )
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Task<List<TEntity>> GetAll ( )
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Task<TEntity> Update ( TEntity entity )
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Task<TEntity> Update ( int id )
+        {
+            throw new NotImplementedException ();
+        }
     }
 }
