62a19cb9377e4ed969649e58244e63665f861a44
gerihal <gerahernan.al@gmail.com>
2020-10-07 12:14:34 +0200

    Added Irepository interface with methods

---
+++ ITProject-Battleships/Data/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using ITProject_Battleships.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITProject_Battleships.Data
+{
+    public interface IRepository<T> where T:class, IEntity
+    {
+        Task<List<T>> GetAll ( );
+        Task<T> Get ( int id );
+        Task<T> Add ( T entity );
+        Task<T> Update ( T entity );
+        Task<T> Update ( int id );
+
+    }
+}
