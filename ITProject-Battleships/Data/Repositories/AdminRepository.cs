8b1d160c373f1db4584f81226eef4481d8c8de46
gerihal <gerahernan.al@gmail.com>
2020-10-07 15:31:28 +0200

    added AdminRepository

--- ITProject-Battleships/Data/Repositories/AdminRepository.cs
+++ ITProject-Battleships/Data/Repositories/AdminRepository.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using ITProject_Battleships.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ITProject_Battleships.Data.Repositories
 {
-    public class AdminRepository
+    public class AdminRepository : GenericRepository<Admin, BattleContext>
     {
+        public AdminRepository (BattleContext context ) : base(context)
+        {
+
+        }
+        // We can add new method specific to the admin repository
     }
 }
