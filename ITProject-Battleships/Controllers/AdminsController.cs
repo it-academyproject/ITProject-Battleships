9d48b1e770d6c9586fcd9af79569109f8a145d3e
Francisco Gil Mayo <francisco.gil.mayo@gmail.com>
2020-10-07 17:21:23 +0200

    B-15 repository

--- ITProject-Battleships/Controllers/AdminsController.cs
+++ ITProject-Battleships/Controllers/AdminsController.cs
@@ -6,104 +6,18 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ITProject_Battleships.Models;
+using ITProject_Battleships.Data.Repositories;
 
 namespace ITProject_Battleships.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AdminsController : ControllerBase
+    public class AdminsController : BattleController<Admin, AdminRepository>
     {
-        private readonly BattleContext _context;
 
-        public AdminsController(BattleContext context)
+        public AdminsController(AdminRepository repository) : base(repository)
         {
-            _context = context;
-        }
 
-        // GET: api/Admins
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Admin>>> GetAdmins()
-        {
-            return await _context.Admins.ToListAsync();
-        }
-
-        // GET: api/Admins/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Admin>> GetAdmin(int id)
-        {
-            var admin = await _context.Admins.FindAsync(id);
-
-            if (admin == null)
-            {
-                return NotFound();
-            }
-
-            return admin;
-        }
-
-        // PUT: api/Admins/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutAdmin(int id, Admin admin)
-        {
-            if (id != admin.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(admin).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!AdminExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Admins
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPost]
-        public async Task<ActionResult<Admin>> PostAdmin(Admin admin)
-        {
-            _context.Admins.Add(admin);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetAdmin", new { id = admin.Id }, admin);
-        }
-
-        // DELETE: api/Admins/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Admin>> DeleteAdmin(int id)
-        {
-            var admin = await _context.Admins.FindAsync(id);
-            if (admin == null)
-            {
-                return NotFound();
-            }
-
-            _context.Admins.Remove(admin);
-            await _context.SaveChangesAsync();
-
-            return admin;
-        }
-
-        private bool AdminExists(int id)
-        {
-            return _context.Admins.Any(e => e.Id == id);
         }
     }
 }
---
+++ ITProject-Battleships/Controllers/BattleController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ITProject_Battleships.Data;
+using ITProject_Battleships.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITProject_Battleships.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public abstract class BattleController<TEntity, TRepository> : ControllerBase
+         where TEntity : class, IEntity
+         where TRepository : IRepository<TEntity>
+    {
+        private readonly TRepository repository;
+
+        public BattleController(TRepository repository)
+        {
+            this.repository = repository;
+        }
+
+
+        // GET: api/[controller]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TEntity>>> Get()
+        {
+            return await repository.GetAll();
+        }
+
+        // GET: api/[controller]/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TEntity>> Get(int id)
+        {
+            var item = await repository.Get(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+
+        // PUT: api/[controller]/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, TEntity item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+            await repository.Update(item);
+            return NoContent();
+        }
+
+        // POST: api/[controller]
+        [HttpPost]
+        public async Task<ActionResult<TEntity>> Post(TEntity item)
+        {
+            await repository.Add(item);
+            return CreatedAtAction("Get", new { id = item.Id }, item);
+        }
+
+        // DELETE: api/[controller]/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<TEntity>> Delete(int id)
+        {
+            var item = await repository.Delete(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+
+    }
+}
